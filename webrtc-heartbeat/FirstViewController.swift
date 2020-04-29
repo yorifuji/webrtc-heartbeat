88dc754bc7b2bcf1a62484433b2ca130f94de681
yorifuji <yorifuji@gmail.com>
2020-04-29 19:10:44 +0900

    Initial Commit

---
+++ webrtc-heartbeat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  webrtc-heartbeat
+//
+//  Created by yorifuji on 2020/04/29.
+//  Copyright © 2020 yorifuji. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ webrtc-heartbeat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  webrtc-heartbeat
+//
+//  Created by yorifuji on 2020/04/29.
+//  Copyright © 2020 yorifuji. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
